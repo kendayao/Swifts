10005d41c710a319aedac8eb856e955fedda0d6a
Kaeneth Dayao <kendayao@gmail.com>
2020-12-17 11:44:20 -0800

    json data created

---
+++ client/src/shop.data.js
@@ -0,0 +1,127 @@
+const SHOP_DATA={
+    casual:{
+        sectionId: 1,
+        title: 'Casual',
+        routeName:'casual',
+        collections: [{
+            c2Swifts:{
+                collectionId: 1,
+                title: 'C2 Swifts',
+                items:[
+                    {
+                    productId: 1,
+                    name: 'C2 Swifts Black',
+                    color: 'Black',
+                    image: 'shoe1-black',
+                    price: '$39.99'
+                    },
+                    {
+                    productId: 2,
+                    name: 'C2 Swifts White',
+                    color: 'White',
+                    image: 'shoe1-white',
+                    price: '$39.99'
+                    },
+                    {
+                    productId: 3,
+                    name: 'C2 Swifts Red',
+                    color: 'Red',
+                    image: 'shoe1-red',
+                    price: '$39.99'
+                    },
+                ]
+            },
+            maxSwifts:{
+                collectionId: 2,
+                title: 'Max Swifts',
+                items:[
+                    {
+                    productId: 4,
+                    name: 'Max Swifts Black',
+                    color: 'Black',
+                    image: 'shoe2-black',
+                    price: '$59.99'
+                    },
+                    {
+                    productId: 5,
+                    name: 'Max Swifts Black-Green',
+                    color: 'Black-Green',
+                    image: 'shoe2-green',
+                    price: '$59.99'
+                    },
+                    {
+                    productId: 6,
+                    name: 'maxSwifts Black-Red',
+                    color: 'Black-Red',
+                    image: 'shoe3-red',
+                    price: '$59.99'
+                    },
+                ]
+            },
+        }]
+    },
+    fitness:{
+        sectionId: 2,
+        title: 'fitness',
+        routeName:'fitness',
+        collections:[{
+            Gymer:{
+                collectionId: 3,
+                title: 'Gymer',
+                items:[
+                    {
+                    productId: 7,
+                    name: 'Gymer Black',
+                    color: 'Black',
+                    image: 'fitness1-black',
+                    price: '$42.99'
+                    },
+                    {
+                    productId: 8,
+                    name: 'Gymer Pink',
+                    color: 'Pink',
+                    image: 'fitness1-pink',
+                    price: '$42.99'
+                    },
+                    {
+                    productId: 9,
+                    name: 'Gymer Purple',
+                    color: 'Purple',
+                    image: 'fitness1-purple',
+                    price: '$42.99'
+                    },
+                    {
+                    productId: 10,
+                    name: 'Gymer Red',
+                    color: 'Red',
+                    image: 'fitness1-red',
+                    price: '$42.99'
+                    }
+                ]
+            },
+            Swifters:{
+                collectionId:4,
+                title: 'Swifters',
+                items:[
+                    {
+                    productId: 11,
+                    name: 'Swifters Black',
+                    color: 'Black',
+                    image: 'fitness3-black',
+                    price: '$29.99'
+                    }
+                ]
+            }
+        }]
+    }
+}
+
+
+
+
+
+
+
+
+
+
